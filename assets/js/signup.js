cb5fc377832aa5d897fed9dd1237e1069154a675
AGPancho03 <agpancho03@gmail.com>
2022-10-22 02:50:22 -0500

    added js code

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,1 +1,6 @@
-console.log("Test")
+let signUp = document.getElementById("login")
+let logIn = document.getElementById("register")
+let signBtn = document.getElementById("signUp")
+
+signBtn.addEventListener("click", function () {
+})
